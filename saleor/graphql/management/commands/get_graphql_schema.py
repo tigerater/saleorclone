4400dec730b6a226a365b281c6f4306ef7830818
tigerater <tigerater@gmail.com>
2020-03-09 02:14:31 +0000

    14

--- saleor/graphql/management/commands/get_graphql_schema.py
+++ saleor/graphql/management/commands/get_graphql_schema.py
@@ -8,10 +8,4 @@ class Command(BaseCommand):
     help = "Writes SDL for GraphQL API schema to stdout"
 
     def handle(self, *args, **options):
-        printed_schema = print_schema(schema)
-        schema_lines = printed_schema.splitlines()
-        for index, line in enumerate(schema_lines):
-            if "implements" in line:
-                schema_lines[index] = line.replace(",", " &")
-        printed_schema = "\n".join(schema_lines)
-        self.stdout.write(printed_schema)
+        self.stdout.write(print_schema(schema))

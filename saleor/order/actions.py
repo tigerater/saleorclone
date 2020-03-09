ffe43cd6711bd5cf521f342f7159c40eb0ad06c7
tigerater <tigerater@gmail.com>
2020-03-09 01:49:22 +0000

    282

--- saleor/order/actions.py
+++ saleor/order/actions.py
@@ -3,7 +3,6 @@ from decimal import Decimal
 from typing import TYPE_CHECKING, List
 
 from django.db import transaction
-from django.utils.translation import pgettext_lazy
 
 from ..core import analytics
 from ..extensions.manager import get_extensions_manager
@@ -196,7 +195,7 @@ def mark_order_as_paid(order: "Order", request_user: "User"):
     )
     payment.charge_status = ChargeStatus.FULLY_CHARGED
     payment.captured_amount = order.total.gross.amount
-    payment.save(update_fields=["captured_amount", "charge_status", "modified"])
+    payment.save(update_fields=["captured_amount", "charge_status"])
 
     events.order_manually_marked_as_paid_event(order=order, user=request_user)
     manager = get_extensions_manager()
@@ -207,12 +206,7 @@ def mark_order_as_paid(order: "Order", request_user: "User"):
 def clean_mark_order_as_paid(order: "Order"):
     """Check if an order can be marked as paid."""
     if order.payments.exists():
-        raise PaymentError(
-            pgettext_lazy(
-                "Mark order as paid validation error",
-                "Orders with payments can not be manually marked as paid.",
-            )
-        )
+        raise PaymentError("Orders with payments can not be manually marked as paid.",)
 
 
 def fulfill_order_line(order_line, quantity):

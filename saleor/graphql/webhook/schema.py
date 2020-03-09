ac08aea2a95e0c0658fd1de85326e3630e6e5a96
tigerater <tigerater@gmail.com>
2020-03-09 02:11:55 +0000

    42

--- saleor/graphql/webhook/schema.py
+++ saleor/graphql/webhook/schema.py
@@ -3,7 +3,7 @@ import graphene
 from ...core.permissions import WebhookPermissions
 from ..core.fields import FilterInputConnectionField
 from ..decorators import permission_required
-from .enums import WebhookSampleEventTypeEnum
+from .enums import WebhookEventTypeEnum
 from .filters import WebhookFilterInput
 from .mutations import WebhookCreate, WebhookDelete, WebhookUpdate
 from .resolvers import (
@@ -37,7 +37,7 @@ class WebhookQueries(graphene.ObjectType):
     webhook_sample_payload = graphene.Field(
         graphene.JSONString,
         event_type=graphene.Argument(
-            WebhookSampleEventTypeEnum,
+            WebhookEventTypeEnum,
             required=True,
             description="Name of the requested event type.",
         ),

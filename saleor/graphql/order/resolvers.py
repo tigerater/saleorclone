f89a024931d0254ccd1e2871a0895d2612a6fb52
tigerater <tigerater@gmail.com>
2020-03-09 02:05:29 +0000

    110

--- saleor/graphql/order/resolvers.py
+++ saleor/graphql/order/resolvers.py
@@ -1,6 +1,7 @@
 import graphene
 import graphene_django_optimizer as gql_optimizer
 
+from ...core.permissions import OrderPermissions
 from ...order import OrderStatus, models
 from ...order.events import OrderEvents
 from ...order.models import OrderEvent
@@ -51,7 +52,12 @@ def resolve_orders_total(_info, period):
 
 
 def resolve_order(info, order_id):
-    return graphene.Node.get_node_from_global_id(info, order_id, Order)
+    """Return order only for user assigned to it or proper staff user."""
+    user = info.context.user
+    order = graphene.Node.get_node_from_global_id(info, order_id, Order)
+    if user.has_perm(OrderPermissions.MANAGE_ORDERS) or order.user == user:
+        return order
+    return None
 
 
 def resolve_homepage_events():
@@ -65,8 +71,4 @@ def resolve_homepage_events():
 
 
 def resolve_order_by_token(token):
-    return (
-        models.Order.objects.exclude(status=OrderStatus.DRAFT)
-        .filter(token=token)
-        .first()
-    )
+    return models.Order.objects.filter(token=token).first()

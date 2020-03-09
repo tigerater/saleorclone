64750363c388941c3f508c72ce2156fd9c43c01c
tigerater <tigerater@gmail.com>
2020-03-09 02:11:32 +0000

    46

--- saleor/webhook/event_types.py
+++ saleor/webhook/event_types.py
@@ -14,8 +14,6 @@ class WebhookEventType:
     ORDER_CANCELLED = "order_cancelled"
     ORDER_FULFILLED = "order_fulfilled"
 
-    FULFILLMENT_CREATED = "fulfillment_created"
-
     CUSTOMER_CREATED = "customer_created"
 
     PRODUCT_CREATED = "product_created"
@@ -32,7 +30,6 @@ class WebhookEventType:
         CUSTOMER_CREATED: "Customer created",
         PRODUCT_CREATED: "Product created",
         CHECKOUT_QUANTITY_CHANGED: "Checkout quantity changed",
-        FULFILLMENT_CREATED: "Fulfillment_created",
     }
 
     CHOICES = [
@@ -45,7 +42,6 @@ class WebhookEventType:
         (CUSTOMER_CREATED, DISPLAY_LABELS[CUSTOMER_CREATED]),
         (PRODUCT_CREATED, DISPLAY_LABELS[PRODUCT_CREATED]),
         (CHECKOUT_QUANTITY_CHANGED, DISPLAY_LABELS[CHECKOUT_QUANTITY_CHANGED]),
-        (FULFILLMENT_CREATED, DISPLAY_LABELS[FULFILLMENT_CREATED]),
     ]
 
     PERMISSIONS = {
@@ -57,5 +53,4 @@ class WebhookEventType:
         CUSTOMER_CREATED: AccountPermissions.MANAGE_USERS,
         PRODUCT_CREATED: ProductPermissions.MANAGE_PRODUCTS,
         CHECKOUT_QUANTITY_CHANGED: CheckoutPermissions.MANAGE_CHECKOUTS,
-        FULFILLMENT_CREATED: OrderPermissions.MANAGE_ORDERS,
     }

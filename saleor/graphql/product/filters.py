c00b28c100e06de15dd1a548c2121142856fa8a2
tigerater <tigerater@gmail.com>
2020-03-09 01:42:08 +0000

    357

--- saleor/graphql/product/filters.py
+++ saleor/graphql/product/filters.py
@@ -256,7 +256,6 @@ class CollectionFilter(django_filters.FilterSet):
     search = django_filters.CharFilter(
         method=filter_fields_containing_value("slug", "name")
     )
-    ids = GlobalIDMultipleChoiceFilter(field_name="id")
 
     class Meta:
         model = Collection
@@ -267,7 +266,6 @@ class CategoryFilter(django_filters.FilterSet):
     search = django_filters.CharFilter(
         method=filter_fields_containing_value("slug", "name", "description")
     )
-    ids = GlobalIDMultipleChoiceFilter(field_name="id")
 
     class Meta:
         model = Category

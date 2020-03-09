4d998021227a3219141a8fe1c7828865cb1e2fd9
tigerater <tigerater@gmail.com>
2020-03-09 01:57:10 +0000

    199

--- saleor/graphql/views.py
+++ saleor/graphql/views.py
@@ -3,9 +3,7 @@ import logging
 import traceback
 from typing import Any, Dict, List, Optional, Tuple, Union
 
-import opentracing
 from django.conf import settings
-from django.db import connection
 from django.http import HttpRequest, HttpResponseNotAllowed, JsonResponse
 from django.shortcuts import render_to_response
 from django.urls import reverse
@@ -26,22 +24,6 @@ API_PATH = SimpleLazyObject(lambda: reverse("api"))
 
 unhandled_errors_logger = logging.getLogger("saleor.graphql.errors.unhandled")
 handled_errors_logger = logging.getLogger("saleor.graphql.errors.handled")
-
-
-def tracing_wrapper(execute, sql, params, many, context):
-    with opentracing.global_tracer().start_span(operation_name="query") as span:
-        span.set_tag("component", "db")
-        span.set_tag("db.statement", sql)
-        span.set_tag("db.many", many)
-        try:
-            result = execute(sql, params, many, context)
-        except Exception as e:
-            span.set_tag("error", True)
-            span.set_tag("error.object", e)
-            raise
-        else:
-            span.set_tag("error", False)
-            return result
 
 
 class GraphQLView(View):
@@ -121,17 +103,6 @@ class GraphQLView(View):
     def get_response(
         self, request: HttpRequest, data: dict
     ) -> Tuple[Optional[Dict[str, List[Any]]], int]:
-        with opentracing.global_tracer().start_span(operation_name="request") as span:
-            span.set_tag("component", "http")
-            span.set_tag("http.method", request.method)
-            span.set_tag("http.path", request.path)
-            result, status_code = self._get_response(request, data)
-            span.set_tag("http.status_code", status_code)
-            return result, status_code
-
-    def _get_response(
-        self, request: HttpRequest, data: dict
-    ) -> Tuple[Optional[Dict[str, List[Any]]], int]:
         execution_result = self.execute_graphql_request(request, data)
         status_code = 200
         if execution_result:
@@ -193,15 +164,14 @@ class GraphQLView(View):
             # executor is not a valid argument in all backends
             extra_options["executor"] = self.executor
         try:
-            with connection.execute_wrapper(tracing_wrapper):
-                return document.execute(  # type: ignore
-                    root=self.get_root_value(),
-                    variables=variables,
-                    operation_name=operation_name,
-                    context=request,
-                    middleware=self.middleware,
-                    **extra_options,
-                )
+            return document.execute(  # type: ignore
+                root=self.get_root_value(),
+                variables=variables,
+                operation_name=operation_name,
+                context=request,
+                middleware=self.middleware,
+                **extra_options,
+            )
         except Exception as e:
             return ExecutionResult(errors=[e], invalid=True)
 
